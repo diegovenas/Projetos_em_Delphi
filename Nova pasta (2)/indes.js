4d8f1e7606285b2b72b25d540fbb58d6c40a634c
diegovenas <72036791+diegovenas@users.noreply.github.com>
2023-03-14 12:42:37 -0300

    Projeto Estudio da bia

---
+++ Nova pasta (2)/indes.js
@@ -0,0 +1,7 @@
+function mostrarTipoCaixa () {
+  document.querySelector('.descricaoCaixa').style.display='block'
+}
+
+function tirarTipoCaixa () {
+  document.querySelector('.descricaoCaixa').style.display='none'
+}
